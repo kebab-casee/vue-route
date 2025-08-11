4ee8e79927a695318f6c4b020725d2cb65be59f0
kebab-casee <bombelatz@gmail.com>
2025-08-11 22:31:51 +0800

    Justine Nabunturan

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-imports.d.ts",
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
